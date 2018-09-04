c0be47fad1f5ed840a6418a01b64da3fbf734233
Allen_Work <allenwangook@gmail.com>
2018-09-04 15:55:34 +0800

    show the picture

--- src/ml/__fruit_analyse__.py
+++ src/ml/__fruit_analyse__.py
@@ -8,14 +8,27 @@ import pandas as pd
 #matplotlib 是一个 Python 的 2D绘图库
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pylab as pl
 
 fruits=pd.read_table('fruit_data_with_colors.txt')
-print("展示数据的前五行:")
-print(fruits.head(5))
-print("看看水果名称有哪些?:")
-print(fruits['fruit_name'].unique())
-print("看看数据的特征:在数据集中有59个水果和7个特征:")
-print(fruits.shape)
+# print("展示数据的前五行:")
+# print(fruits.head(5))
+# print("看看水果名称有哪些?:")
+print(fruits['height'].unique())
+# print("看看数据的特征:在数据集中有59个水果和7个特征:")
+# print(fruits.shape)
 #数据可视化工具显示
-sns.countplot(fruits['fruit_name'],label="Count")
-plt.show()
+# sns.countplot(fruits['fruit_name'],label="Count")
+# plt.show()
+'''箱型图'''
+# fruits.drop('fruit_label', axis=1).plot(kind='box', subplots=True, layout=(2,2), 
+#                                         sharex=False, sharey=False, figsize=(9,9),
+#                                         title='Box Plot for each input variable')
+# plt.savefig('fruits_box')
+# plt.show()
+
+'''柱状图'''
+# fruits.drop("fruit_label", axis=1).hist(bins=30,figsize=(9,9))
+# pl.suptitle("Histogram for each numeric input variable")
+# plt.savefig('fruits_hist')
+# plt.show()
